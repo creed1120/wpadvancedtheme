b742a0231c4a6e289089113763051f47abc1e4e9
creed1120 <cedricareed@gmail.com>
2023-02-20 00:04:03 -0700

    added theme support

--- includes/classes/class-wpadvanced-theme.php
+++ includes/classes/class-wpadvanced-theme.php
@@ -25,7 +25,19 @@ class WPADVANCED_THEME {
         /**
          * Actions
          */
+         add_action('after_setup_theme', [ $this, 'theme_setup' ]);
+    }
 
+    public function theme_setup() {
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'custom-logo', [
+            'header-text'          => array( 'site-title', 'site-description' ),
+            'height'               => 100,
+            'width'                => 400,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'unlink-homepage-logo' => true,
+        ] );
     }
 
 }
